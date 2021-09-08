48af7fcb3214b325aad708d432742465fe3878b5
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2021-09-08 21:23:09 +0300

    Add files via upload

---
+++ currencies.lua
@@ -0,0 +1,24 @@
+local currencies = {}
+
+local function addCurrency(name, id, dmg, model, color, max)
+    table.insert(currencies, {
+        name = name,
+        id = id,
+        dmg = dmg,
+        model = model,
+        color = color,
+        max = max,
+    })
+end
+
+addCurrency("Деньги",           "customnpcs:npcMoney",      0,   'INGOT', 0x85BB65, nil)
+--addCurrency("Светопыль",        "minecraft:glowstone_dust", 0,   'DUST',  0xD0D000, 5)
+--addCurrency("Железный слиток",  "minecraft:iron_ingot",     0,   'INGOT', 0xAAAAAA, nil)
+--addCurrency("Железный блок",    "minecraft:iron_block",     0,   'BLOCK', 0xAAAAAA, 6)
+--addCurrency("Медный слиток",    "IC2:itemIngot",            0,   'INGOT', 0xA5642F, nil)
+--addCurrency("Медный блок",      "Forestry:resourceStorage", 1,   'BLOCK', 0xA5642F, 6)
+--addCurrency("Оловянный слиток", "IC2:itemIngot",            1,   'INGOT', 0xCCCCCC, nil)
+--addCurrency("Оловянный блок",   "Forestry:resourceStorage", 2,   'BLOCK', 0xCCCCCC, 6)
+--addCurrency("Бесплатно",        nil,                        nil, nil,     0xE5E5E5, nil)
+
+return currencies

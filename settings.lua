20bac3799989aff8e12ce8297b91719e97c8c0d1
thedark1232 <72810871+thedark1232@users.noreply.github.com>
2021-09-08 21:10:36 +0300

    Add files via upload

---
+++ settings.lua
@@ -0,0 +1,13 @@
+local settings = {}
+
+--settings.REPOSITORY = "https://raw.githubusercontent.com/salimAbdulIt/OpenComputers-Casino/master"
+settings.REPOSITORY = "https://raw.githubusercontent.com/thedark1232/MyCasino/main/"
+settings.TITLE = "Приветствуем ваc у нас в казино"
+settings.ADMINS = {"The_Dark1232"}
+
+-- CHEST / PIM / ...
+settings.PAYMENT_METHOD = "CHEST"
+settings.CONTAINER_PAY = "DOWN"
+settings.CONTAINER_GAIN = "UP"
+
+return settings;
